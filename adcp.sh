bbc31bf4fe039b3e0b4680dcf4ef0a1431e2c71d
ymm10ak1 <66200742+ymm10ak1@users.noreply.github.com>
2025-01-22 23:39:39 +0900

    update: 今いるディレクトリでacc addする際に引数を渡さずにコマンドを使用したいためデフォルト値ありの変数に格納するように変更

--- adcp.sh
+++ adcp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-  echo "usage: adcp <hierarchy-up-number>"
-  echo "実行するには1個の引数が必要です"
-elif [ "$1" -eq 0 ]; then
+# 何個前のディレクトリに戻るか(デフォルトは0)
+BACK_DIR="${1:-0}"
+
+if [ "$BACK_DIR" -eq 0 ]; then
   acc add --template cpp
-elif [ "$1" -eq 1 ]; then
+elif [ "$BACK_DIR" -eq 1 ]; then
   cd .. && acc add --template cpp
-elif [ "$1" -eq 2 ]; then
+elif [ "$BACK_DIR" -eq 2 ]; then
   cd ../.. && acc add --template cpp
 fi
